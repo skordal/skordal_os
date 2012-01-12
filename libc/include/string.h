3ddc3e658d85f7efdb75f42fb89e32b2d6ff979d
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2012-01-12 09:15:12 +0100

    Improved the C library functions.
    Implemented the memcpy and mempcpy functions.
    Replaced some sub+cmp instructions with subs.
    Corrected a spelling mistake in string.h.

--- libc/include/string.h
+++ libc/include/string.h
@@ -1,7 +1,7 @@
 // SkordalOS C Library
 // (c) Kristian K. Skordal 2011 - 2012
 
-// This file contains standard C string functions.
+// This file contains the standard C string functions.
 
 #ifndef __SOS_STRING_H__
 #define __SOS_STRING_H__
@@ -11,12 +11,12 @@
 // String length:
 size_t strlen(const char * string);
 
-// String copying:
+// String copy functions:
 char * strcpy(char * dest, const char * source);
 char * strncpy(char * dest, const char * source, size_t length);
 
 // Fills the specified memory area with a constant:
-void * memset(void * memory_area, int constand, size_t length);
+void * memset(void * memory_area, int constant, size_t length);
 
 // Copies memory from one location to another:
 void * memcpy(void * destination, const void * source, size_t length);
